c60cecdb473ec56331115b11a8c4dd2aadaaa41c
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-07-30 20:22:22 +0300

    createServer

--- http-server.js
+++ http-server.js
@@ -5,8 +5,8 @@ const serve = require('serve')
 
 module.exports = ({ router, port=80, host='0.0.0.0', key=null, cert=null }) => {
 	const server = (key && cert) ?
-		https.startServer({ key, cert }, handler(router)) :
-		http.startServer(handler(router))
+		https.createServer({ key, cert }, handler(router)) :
+		http.createServer(handler(router))
 	server.listen(port, host)
 	return server
 }
