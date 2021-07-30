1c50d54b425cf945be2179e2a80f0822fb953b37
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-07-30 20:12:38 +0300

    initial commit

---
+++ http-server.js
@@ -0,0 +1,18 @@
+'use strict'
+const http = require('http')
+const https = require('https')
+const serve = require('serve')
+
+module.exports = ({ router, port=80, host='0.0.0.0', key=null, cert=null }) => {
+	const server = (key && cert) ?
+		https.startServer({ key, cert }, handler(router)) :
+		http.startServer(handler(router))
+	server.listen(port, host)
+	return server
+}
+
+const handler = router => (request, socket) => {
+	router(request)
+	.catch(e => ({ status: 500, mimetype: 'text/plain', data: e.message, headers: [] }))
+	.then(serve(socket))
+}
